c394e005bb17d4b40d7b865bb87254f7382079d8
6GzkWt3AF75q <mymalwareemail@yahoo.com>
2017-05-18 01:25:18 +0800

    lAT commit

--- app.js
+++ app.js
@@ -7,11 +7,20 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+const users = require('./routes/users');
+
 const port = 3000;
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.use('/users', users); 
+// if we have www.xxx.com/users/moshe then moshe will go to users
 
 app.listen(port, function(){
     console.log('Server started on port ' + port);
-//npm install -g nodemon
+//npm install -g nodemon - export PATH=${PATH}:/Users/tal/.node/bin/
 
 app.get('/', function(req, res){
     res.send('Invalid Endpoint');
